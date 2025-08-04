6ea0e0a0494d43fcb0512646c5dc9ad74326cf60
radenmasabdul <abdul.tamsis@gmail.com>
2025-08-04 22:33:10 +0700

    feat: add slicing navbar & dark mode

--- src/components/Layout/Drawer.jsx
+++ src/components/Layout/Drawer.jsx
@@ -7,15 +7,17 @@ const Drawer = () => {
 
   return (
     <div
-      className={`bg-base-200 transition-all duration-300 h-full p-4 ${
+      className={`bg-white transition-all duration-300 h-screen p-4 dark:bg-gray-800 ${
         collapsed ? "w-20" : "w-64"
       }`}
     >
       <div className="flex justify-between items-center mb-6">
-        {!collapsed && <span className="font-bold text-xl">SkyTrack</span>}
+        {!collapsed && (
+          <span className="font-bold text-xl dark:text-gray-100">SkyTrack</span>
+        )}
         <button
           onClick={() => setCollapsed(!collapsed)}
-          className="btn btn-ghost btn-sm"
+          className="btn btn-ghost btn-sm dark:text-gray-100"
         >
           {collapsed ? <ChevronRight size={20} /> : <ChevronLeft size={20} />}
         </button>
@@ -23,7 +25,10 @@ const Drawer = () => {
 
       <ul className="menu space-y-2">
         <li>
-          <Link to="/dashboard" className="flex items-center gap-2">
+          <Link
+            to="/dashboard"
+            className="flex items-center gap-2 dark:text-gray-100"
+          >
             <Menu size={18} />
             {!collapsed && "Dashboard"}
           </Link>
--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Wind, Cloud, Leaf } from "lucide-react";
 import Swal from "sweetalert2";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [activeMenu, setActiveMenu] = useState("udara");
+  const userString = localStorage.getItem("user");
+  const name = userString ? JSON.parse(userString).name : "";
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "dark") {
+      document.documentElement.classList.add("dark");
+      setIsDarkMode(true);
+    }
+  }, []);
+
+  const toggleDarkMode = () => {
+    const root = document.documentElement;
+    if (isDarkMode) {
+      root.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+      setIsDarkMode(false);
+    } else {
+      root.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+      setIsDarkMode(true);
+    }
+  };
 
   const handleLogout = () => {
     Swal.fire({
@@ -15,58 +42,141 @@ const Navbar = () => {
 
     setTimeout(() => {
       localStorage.removeItem("user");
+      localStorage.removeItem("name");
       navigate("/");
     }, 1300);
   };
 
+  const menuClasses = (menuName) =>
+    `flex items-center gap-2 text-xl font-medium cursor-pointer px-2 pb-1 transition ${
+      activeMenu === menuName
+        ? "border-b-2 border-blue-500 text-blue-600 dark:text-blue-400"
+        : "text-black dark:text-gray-100"
+    }`;
+
   return (
-    <>
-      <div className="navbar bg-base-100 shadow-sm">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
-        </div>
-        <div className="flex gap-2">
-          <input
-            id="search"
-            name="search"
-            type="text"
-            placeholder="Search"
-            className="input input-bordered w-24 md:w-auto"
-          />
-          <div className="dropdown dropdown-end">
-            <div
-              tabIndex="0"
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
+    <div className="navbar bg-base-100 shadow-sm px-4 dark:bg-gray-800">
+      {/* kiri */}
+      <div className="navbar-start gap-4">
+        <button
+          className={menuClasses("udara")}
+          onClick={() => setActiveMenu("udara")}
+        >
+          <Wind size={18} />
+          Inventarisasi Udara
+        </button>
+        <button
+          className={menuClasses("emisi")}
+          onClick={() => setActiveMenu("emisi")}
+        >
+          <Cloud size={18} />
+          Inventarisasi Emisi
+        </button>
+      </div>
+
+      {/* tengah */}
+      <div className="navbar-center hidden lg:flex">
+        <button className="flex items-center gap-2 text-lg font-semibold dark:text-gray-100">
+          <Leaf size={20} />
+          Skytrack Monitor
+        </button>
+      </div>
+
+      {/* kanan */}
+      <div className="navbar-end flex items-center gap-4">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Cari ID Stasiun"
+          className="input input-sm input-bordered w-32 md:w-52 dark:white"
+        />
+
+        <button className="relative p-2 rounded-full cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5 dark:text-gray-100"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+            />
+          </svg>
+          <span className="badge badge-xs badge-error absolute top-0 right-0"></span>
+        </button>
+
+        {/* dropdown */}
+        <div className="dropdown dropdown-end">
+          <div tabIndex={0} className="flex items-center gap-2 cursor-pointer">
+            <div className="w-8 h-8 rounded-full overflow-hidden">
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                alt="avatar"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <span className="text-sm font-medium dark:text-gray-100">
+              {name}
+            </span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4 dark:text-gray-100"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52 dark:bg-gray-800"
+          >
+            <li>
+              <a className="dark:text-gray-100 hover:bg-blue-500 hover:text-white">
+                Profile
+              </a>
+            </li>
+            <li>
+              <a className="dark:text-gray-100 hover:bg-blue-500 hover:text-white">
+                Settings
+              </a>
+            </li>
+            <li>
+              <div className="flex items-center justify-between px-2 dark:text-gray-100 hover:bg-blue-500 hover:text-white rounded-md">
+                <span>Dark Mode</span>
+                <input
+                  type="checkbox"
+                  id="toggle"
+                  name="toggle"
+                  className="toggle toggle-sm"
+                  checked={isDarkMode}
+                  onChange={toggleDarkMode}
                 />
               </div>
-            </div>
-            <ul
-              tabIndex="0"
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li>
-                <a>Settings</a>
-              </li>
-              <li>
-                <a onClick={handleLogout}>Logout</a>
-              </li>
-            </ul>
-          </div>
+            </li>
+            <li>
+              <a
+                onClick={handleLogout}
+                className="dark:text-gray-100 hover:bg-blue-500 hover:text-white"
+              >
+                Logout
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
