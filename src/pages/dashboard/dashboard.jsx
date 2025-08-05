e2c38bc7598928dbe4f4f63d72c463dce3f89e3f
radenmasabdul <abdul.tamsis@gmail.com>
2025-08-05 22:10:34 +0700

    feat: default map to dark

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -6,14 +6,14 @@ const Dashboard = () => {
     <>
       <MapContainer
         center={[-6.2, 106.8]}
-        zoom={12}
+        zoom={11}
         style={{ height: "100vh", width: "100%" }}
       >
         <LayersControl position="topleft">
           <LayersControl.BaseLayer checked name="Default Map">
             <TileLayer
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              attribution="&copy; OpenStreetMap contributors"
+              url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+              attribution='&copy; <a href="https://carto.com/">CARTO</a>'
             />
           </LayersControl.BaseLayer>
           <LayersControl.BaseLayer name="Satellite">
